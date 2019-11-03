32c5a10e5c5f58b9ce8ee353be017104590e478b
Fernando <fernandomellone2@gmail.com>
2019-11-02 21:39:05 -0300

    se mejoró el frontend, se cambió la navbar por una sidebar, sólo se modificó el index. Se agregaron archivos con los estilos para la sidebar.

--- FormularioAlumno.php
+++ FormularioAlumno.php
@@ -11,29 +11,9 @@
 <head>
 
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarColor01">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item">
-        <a class="nav-link" href="index.php">Home </a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="FormularioAlumno.php">Alumnos <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="FormularioProfesor.php">Profesores</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="index.php">Inscripción <span class="sr-only">(current)</span></a>
-      </li>
-    </ul>
-  </div>
-</nav>
+<div class="wrapper">
+    
+</div>
 <h1>Alumno</h1>
 <?php
     if(isset($_GET["idAlumno"])) {
---
+++ includes/_header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Escuela Fulanito</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/estilo.css">
+
+  </head>
+  <body>
+
+    <div class="d-flex" id="wrapper">
+
+      <!-- Sidebar -->
+      <div class="bg-light border-right" id="sidebar-wrapper">
+        <div class="sidebar-heading"><b>Opciones </b> </div>
+        <div class="list-group list-group-flush">
+          <a href="./FormularioAlumno.php" class="list-group-item list-group-item-action bg-light">Alumnos</a>
+          <a href="./FormularioProfesor.php" class="list-group-item list-group-item-action bg-light">Profesores</a>
+          <a href="#" class="list-group-item list-group-item-action bg-light">Inscripciones</a>
+          <a href="#" class="list-group-item list-group-item-action bg-light">Cobros</a>
+          <a href="#" class="list-group-item list-group-item-action bg-light">Profile</a>
+          <a href="#" class="list-group-item list-group-item-action bg-light">Status</a>
+        </div>
+      </div>
+      <!-- /#sidebar-wrapper -->
+
+      <!-- Page Content -->
+      <div id="page-content-wrapper">
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+          <button class="navbar-toggler" id="menu-toggle" type="button" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                </div>
+              </li>
+            </ul>
+          </div>
+        </nav>
